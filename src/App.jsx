04d270dece5ae40c182ed37e71194a6d2171cbd3
Meherrem ALiyev <maharram.aliyev98@gmail.com>
2025-03-06 21:31:20 +0400

    routing test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/public/LandingPage";
 import './styles/style.scss'
 import SingUp from "./pages/auth/SingUp";
+import Test from "./components/auth/login/Test";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signUp" element={<SingUp />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </div>
   )
